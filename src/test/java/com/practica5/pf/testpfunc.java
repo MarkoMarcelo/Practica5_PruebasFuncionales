38447ada1652d1dac4112dc480c4efbebc70ac5e
MarkoMarcelo <83055437+MarkoMarcelo@users.noreply.github.com>
2022-11-11 20:44:11 -0500

    Add files via upload

---
+++ src/test/java/com/practica5/pf/testpfunc.java
@@ -0,0 +1,48 @@
+package com.practica5.pf;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testpfunc {
+
+	private WebDriver driver;
+	
+
+	@Test
+    public void eightComponents() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        assertEquals("Web form", title);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        assertEquals("Received!", value);
+
+        //driver.quit();
+    }
+	
+	@After
+	public void tearDown() {
+		//driver.quit();
+	}
+}
